3d16283a8fb270d9cb88a4142dd1b3295e3dc990
Vanishree Rao K <vanishree051@gmail.com>
2026-06-11 15:17:21 +0530

    Added headless execution support for GitHub Actions

--- conftest.py
+++ conftest.py
@@ -10,7 +10,16 @@ def setup(request):
     browser = request.param
 
     if browser == "chrome":
-        driver = webdriver.Chrome()
+
+    from selenium.webdriver.chrome.options import Options
+
+    chrome_options = Options()
+
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=chrome_options)
 
     elif browser == "firefox":
         driver = webdriver.Firefox()
